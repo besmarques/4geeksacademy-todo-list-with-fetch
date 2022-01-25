de7e1942c57f1ccda0cc2e949fdc226e7098f392
Bruno Marques <besmarques@gmail.com>
2022-01-25 22:30:12 +0000

    delete not working

--- src/js/component/listsolve3.jsx
+++ src/js/component/listsolve3.jsx
@@ -3,24 +3,59 @@ import React, { useState, useEffect } from "react";
 const List = () => {
 	const url = "https://assets.breatheco.de/apis/fake/todos/user/besmarques";
 	let [toDoList, setToDoList] = useState([]);
-	let ToDoWorkingVar = [];
-	let vardsa = "";
 
-	/*fetch("https://assets.breatheco.de/apis/fake/todos/user/besmarques", {
-		method: "GET",
-		headers: { "Content-Type": "application/json" },
-	})
-		.then((resp) => {
-			return resp.json();
+	function myCreateUsername() {
+		fetch(url, {
+			method: "POST",
+			body: JSON.stringify([]),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+	}
+
+	function getFromOnline() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/besmarques", {
+			method: "GET",
+			headers: { "Content-Type": "application/json" },
 		})
-		.then((data) => {
-			vardsa = JSON.stringify(data);
-			console.log(vardsa);
-			return vardsa;
-		});*/
+			.then((resp) => {
+				if (resp.ok) {
+					return resp.json();
+				} else {
+					myCreateUsername();
+				}
+			})
+			.then((data) => {
+				return data;
+			})
+			.then((treatedData) => {
+				let temp = treatedData.map(({ label }) => {
+					return label;
+				});
+				setToDoList(temp);
+			});
+	}
+
+	function pushToOnline() {
+		let temp = toDoList.map((value) => {
+			return { label: value, done: false };
+		});
+
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/besmarques", {
+			method: "PUT",
+			body: JSON.stringify(temp),
+
+			headers: { "Content-Type": "application/json" },
+		});
+	}
 
 	useEffect(() => {
-		console.log("yeah");
+		getFromOnline();
+	}, []);
+
+	useEffect(() => {
+		pushToOnline();
 	}, [toDoList]);
 
 	function addToDoList() {
@@ -35,6 +70,15 @@ const List = () => {
 		setToDoList(temp);
 	}
 
+	function deleteAllTasks() {
+		setToDoList([]);
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/besmarques", {
+			method: "DELETE",
+
+			headers: { "Content-Type": "application/json" },
+		});
+	}
+
 	return (
 		<>
 			<div>{toDoList}</div>
@@ -44,6 +88,8 @@ const List = () => {
 			<button onClick={() => deleteFromToDoList()}>
 				remove to do list
 			</button>
+
+			<button onClick={() => deleteAllTasks()}>delete all</button>
 		</>
 	);
 };
